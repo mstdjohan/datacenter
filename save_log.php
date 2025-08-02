224373c8c7aee84f945e0924505ddd0362912d01
mstdjohan <miningduta6@gmail.com>
2025-08-02 16:16:00 +0700

    Update save_log.php

--- save_log.php
+++ save_log.php
@@ -1,1 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+  $type = $_POST["type"] ?? 'UNKNOWN';
+  $price = $_POST["price"] ?? '0';
+  $time = $_POST["time"] ?? date('H:i:s');
+
+  $line = "$time - $type @ $price\n";
+  file_put_contents("log_signals.txt", $line, FILE_APPEND);
+  echo "OK";
+} else {
+  echo "Invalid request.";
+}
+?>
 
